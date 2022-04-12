f0efd65eecb005d9ee390e3b98fd409e85aebbc7
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2022-04-12 20:45:35 +0900

    Add:Ability to save and redirect the blog posting

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
 
+Route::post('/posts', 'PostController@store');
+
 /*Route::get('/posts', function() {
     return view('posts/index');
 });*/
